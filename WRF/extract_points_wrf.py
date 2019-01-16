a7f3df5dde4bf4f49e50a60822f4b4ea078a917e
Bruce Crevensten <bruce.crevensten@gmail.com>
2019-01-16 15:25:33 -0800

    add logging and refer to s3 bucket

--- WRF/extract_points_wrf.py
+++ WRF/extract_points_wrf.py
@@ -2,17 +2,39 @@
 An example of how to extract timeseries data from point locations
 using WRF data stored in AWS.
 
-Author: Michael Lindgren (malindgren@alaska.edu), SNAP
+Assumes that the `wrf-ak-ar5` S3 bucket is mounted at `~/wrf-ak-ar5`
+
+Authors: Michael Lindgren (malindgren@alaska.edu), SNAP
 """
 
+# Setup logger to print to STDOUT
+import logging
+import sys
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
 def extract_data():
-    dataset = xr.open_dataset('t2_hourly_wrf_GFDL-CM3_historical_1971.nc')
-    res = 20000 # grid dimension of this WRF data
+    """
 
-    x0, y0 = dataset.xc.min()-(res/2), dataset.yc.max()+(res/2)
+    Open data set, perform extraction, save CSV.
 
+    """
+    log.info('Loading dataset...')
+    dataset = xr.open_dataset('~/wrf-ak-ar5/hourly/GFDL-CM3/historical/t2/t2_hourly_wrf_GFDL-CM3_historical_1971.nc')
+
+    log.info('Dataset loaded, processing...')
+
+    res = 20000 # grid resolution for WRF data
     # get an affine transform to make the point lookups faster
-    affine_dataset = rasterio.transform.from_origin(x0, y0, res, res)
+    affine_dataset = rasterio.transform.from_origin(
+        dataset.xc.min()-(res/2),
+        dataset.yc.max()+(res/2),
+        res, res)
 
     # point locations we are going to extract from the NetCDF file
     # these locations are in WGS1984 EPSG:4326
@@ -24,7 +46,9 @@ def extract_data():
     }
 
     # reproject the points to the wrf-polar-stereo using geopandas
-    location = {location_name:Point(lng_lat) for location_name, lng_lat in location.items()}
+    location = {
+        location_name:Point(lng_lat) for location_name, lng_lat in location.items()
+    }
     dataframe = pd.Series(location).to_frame('geometry')
     wrf_crs = dataset.proj_parameters
     pts_proj = gpd.GeoDataFrame(dataframe, crs={'init':'epsg:4326'}).to_crs(wrf_crs)
@@ -34,22 +58,21 @@ def extract_data():
     for location_name, point in pts_proj.geometry.to_dict().items():
         # get row/col from x/y using affine
         col, row = ~affine_dataset * (point.x, point.y)
-        pprint(col)
-        pprint(row)
         col, row = [int(i) for i in [col, row]]
         #pprint(col, row)
 
         # extract
         extracted[location_name] = dataset['t2'][:, row, col].values # extract t2 = temp at 2m
 
-    pprint(extracted)
+    log.info('Finished processing, writing file...')
+
     # make a dataframe with the extracted outputs
     extracted_df = pd.DataFrame(extracted, index=dataset.time.to_index())
     extracted_df.to_csv('extracted.csv')
+    log.info('...done!')
 
 if __name__ == "__main__":
     from shapely.geometry import Point
-    from pprint import pprint
     import xarray as xr
     import pandas as pd
     import geopandas as gpd
